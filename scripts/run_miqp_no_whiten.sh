2319a90b12dd0858ce261da5bb4482fe183ea1e2
Yanlin Feng <fengyanlin@pku.edu.cn>
2023-04-24 19:01:51 -0400

    Update script

--- scripts/run_miqp_no_whiten.sh
+++ scripts/run_miqp_no_whiten.sh
@@ -1,11 +1,11 @@
 for n in 5 24; do
   for seed in {0..4}; do
-    python -u src/run_few_shot.py -np 1 --n_shot $n --whiten -s cos_topk --eval_subset "val100_seed$seed"
+    python -u src/run_few_shot.py -np 1 --n_shot $n -s cos_topk --eval_subset "val100_seed$seed"
     python -u src/evaluate.py --predictions_json outputs/raw/raw_dev_pred.json
   done
   for alpha in 0.001 0.003 0.01 0.03 0.1 0.3 1; do
     for seed in {0..4}; do
-      python -u src/run_few_shot.py -np 1 --n_shot $n --whiten -s miqp --top_sim 50 --div_alpha $alpha --eval_subset "val100_seed$seed"
+      python -u src/run_few_shot.py -np 1 --n_shot $n -s miqp --top_sim 50 --div_alpha $alpha --eval_subset "val100_seed$seed"
       python -u src/evaluate.py --predictions_json outputs/raw/raw_dev_pred.json
     done
   done
